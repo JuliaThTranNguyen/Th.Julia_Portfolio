87ed7d28acd8ed78527fd63815df85decbe04fbb
JuliaThTranNguyen <trhien541@gmail.com>
2023-11-28 00:51:30 +0200

    28.11v - added: JUJU note app

---
+++ src/constants/items.tsx
@@ -0,0 +1,83 @@
+export const items = [
+    {
+      id: 0,
+      title: "Introduction Website",
+      img: "https://user-images.githubusercontent.com/49017322/232626283-ec6a60c2-cfeb-443d-ae73-1e2cf4e22fe6.png",
+      desc: "This is a simple HTML webpage for self-introduction.",
+      demo:"https://fs16-1-basic-html-bice.vercel.app/",
+      tech:"Technologies: HTML, Formsubmit, Vercel"
+    },
+    {
+      id: 1,
+      title: "Weather App - UI design",
+      img: "https://user-images.githubusercontent.com/49017322/221565215-7fb2c69a-197d-4d26-8e1b-7321613de876.png",
+      desc: "This is a very simple & responsive weather forecast application written in React.",
+      demo:"https://weather-forecast-eight-woad.vercel.app/",
+      tech:"Technologies: ReactJs, REST APIs, Vercel"
+    },
+    {
+      id: 2,
+      title: "Shopping App - UI design",
+      img: "https://user-images.githubusercontent.com/49017322/279810682-5af4b437-50f4-4c64-a161-b7c8008385a7.png",
+      desc: "This is a simple UI design for an e-commerce app.",
+      demo:"https://fs16-6-frontend-project-gamma.vercel.app/",
+      tech:"Technologies: Typescript, Redux Query, Material UI, REST APIs, Vercel"
+    },
+    {
+      id: 3,
+      title: "Maido - UI design",
+      img: "https://user-images.githubusercontent.com/49017322/279810649-8d6b602b-82f1-42f5-8212-e9f6512ac676.png",
+      desc: "This is a copy of the UI design for the Maido website.",
+      demo:"https://fs16-2-responsive-design.vercel.app/",
+      tech:"Technologies: Typescript, SCSS, REST APIs, Vercel"
+    },
+    {
+      id: 4,
+      title: "Warehouse management system ",
+      img: "https://user-images.githubusercontent.com/49017322/279810742-108a2dba-8a53-4b37-9f42-0d23eb294b32.png",
+      desc: "This is an Admin Panel for a warehouse management application.",
+      demo:"https://admin-panel-julia.vercel.app/login",
+      tech:"Technologies: Next.Js, MongoDB & JWT tokens, NextAuth, Google Cloud Console, Amazon S3, TailwindCSS, Vercel"
+    },
+    {
+      id: 5,
+      title: "E-commercial App",
+      img: "https://user-images.githubusercontent.com/49017322/279810762-2d74d466-6a2c-45a0-ba49-5862a0a625a0.png",
+      desc: "This is an e-commerce app, which serves as the frontend application for the admin panel mentioned above.",
+      demo:"https://e-commerce-julia.vercel.app/",
+      tech:"Technologies: Next.Js, Google Cloud Console, TailwindCSS, Stripe Payment, Vercel"
+    },
+    {
+      id: 6,
+      title: "Personal Portfolio",
+      img: "https://user-images.githubusercontent.com/49017322/280490781-3bf7fbb1-1cfc-4000-b1ae-eb35e4c67358.png",
+      desc: "This is a simple personal portfolio.",
+      demo:"https://th-julia-portfolio.vercel.app/",
+      tech:"Technologies: Javascript, SCSS, Vite, EmailJS, Vercel"
+    },
+    {
+      id: 7,
+      title: "Travel Agency",
+      img: "https://user-images.githubusercontent.com/49017322/282851205-ae9f431b-38c2-4e3b-9ea3-7943886c08fa.png",
+      desc: "This is a simple UI/UX design for a travel agency website.",
+      demo:"https://travel-agency-fawn.vercel.app/",
+      tech:"Technologies: Typescript, NextJs, Tailwinds CSS, Vercel"
+    },
+    {
+      id: 8,
+      title: "Shobie Online Shopping",
+      img: "https://user-images.githubusercontent.com/49017322/284315779-f2aa96ee-528b-4cde-a37e-4d771183c4f9.png",
+      desc: "This is a simple UI/UX design for an online shopping website.",
+      demo:"https://ux-ui-shobie.vercel.app/",
+      tech:"Technologies: Typescript, NextJs, Tailwinds CSS, Vercel"
+    },
+    {
+      id: 9,
+      title: "JuJu Note App",
+      img: "https://private-user-images.githubusercontent.com/49017322/286037762-3a651900-c87b-4c45-a7f6-cd877f80113e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDExMjU0MDcsIm5iZiI6MTcwMTEyNTEwNywicGF0aCI6Ii80OTAxNzMyMi8yODYwMzc3NjItM2E2NTE5MDAtYzg3Yi00YzQ1LWE3ZjYtY2Q4NzdmODAxMTNlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzExMjclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMTI3VDIyNDUwN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTJiM2VlZTIzYTZkNTdiZjJkYmNlZTBiOTNlMWYyNDYzNDMwMjZlYzdjNjI0ZjFmMzliOTEyZWU1ODRmNDA2NzYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.xMl_yWgtw-EI5H4RwoKHRKrSH-tAnPk1cFv0vpG7jQw",
+      desc: "This is a simple todo app.",
+      demo:"https://note-app-8y3u.vercel.app/",
+      tech:"Technologies: Typescript, Material UI, SCSS, Vite, Vercel"
+    },
+  ];
+  
