0fa6ef43fa9dbfef110af853736e8bf0afaf4060
Thuy Hien Tran Nguyen <trhien541@gmail.com>
2025-02-03 14:42:11 +0200

    Update items.tsx

--- src/constants/items.tsx
+++ src/constants/items.tsx
@@ -12,7 +12,7 @@ export const items = [
       title: "Weather App - UI design",
       img: "https://user-images.githubusercontent.com/49017322/221565215-7fb2c69a-197d-4d26-8e1b-7321613de876.png",
       desc: "This is a very simple & responsive weather forecast application written in React.",
-      demo:"https://weather-forecast-eight-woad.vercel.app/",
+      demo:"https://weather-forecast-6bsd0dg30-juliathtrannguyens-projects.vercel.app/",
       tech:"Technologies: ReactJs, REST APIs, Vercel"
     },
     {
@@ -20,7 +20,7 @@ export const items = [
       title: "Shopping App - UI design",
       img: "https://user-images.githubusercontent.com/49017322/279810682-5af4b437-50f4-4c64-a161-b7c8008385a7.png",
       desc: "This is a simple UI design for an e-commerce app.",
-      demo:"https://fs16-6-frontend-project-gamma.vercel.app/",
+      demo:"https://fake-store-app-git-master-juliathtrannguyen.vercel.app/",
       tech:"Technologies: Typescript, Redux Query, Material UI, REST APIs, Vercel"
     },
     {
@@ -28,12 +28,12 @@ export const items = [
       title: "Maido - UI design",
       img: "https://user-images.githubusercontent.com/49017322/279810649-8d6b602b-82f1-42f5-8212-e9f6512ac676.png",
       desc: "This is a copy of the UI design for the Maido website.",
-      demo:"https://fs16-2-responsive-design.vercel.app/",
+      demo:"https://fs16-2-responsive-design-abkxjtf8i-juliathtrannguyens-projects.vercel.app/",
       tech:"Technologies: Typescript, SCSS, REST APIs, Vercel"
     },
     {
       id: 4,
-      title: "Warehouse management system ",
+      title: "Warehouse management system - not available",
       img: "https://user-images.githubusercontent.com/49017322/279810742-108a2dba-8a53-4b37-9f42-0d23eb294b32.png",
       desc: "This is an Admin Panel for a warehouse management application.",
       demo:"https://admin-panel-julia.vercel.app/login",
@@ -41,10 +41,10 @@ export const items = [
     },
     {
       id: 5,
-      title: "E-commercial App",
+      title: "E-commercial App - not available",
       img: "https://user-images.githubusercontent.com/49017322/279810762-2d74d466-6a2c-45a0-ba49-5862a0a625a0.png",
       desc: "This is an e-commerce app, which serves as the frontend application for the admin panel mentioned above.",
-      demo:"https://e-commerce-neon-kappa.vercel.app/",
+      demo:"",
       tech:"Technologies: Next.Js, Google Cloud Console, TailwindCSS, Stripe Payment, Vercel"
     },
     {
@@ -60,7 +60,7 @@ export const items = [
       title: "Travel Agency",
       img: "https://user-images.githubusercontent.com/49017322/282851205-ae9f431b-38c2-4e3b-9ea3-7943886c08fa.png",
       desc: "This is a simple UI/UX design for a travel agency website.",
-      demo:"https://travel-agency-fawn.vercel.app/",
+      demo:"https://travel-agency-git-master-juliathtrannguyens-projects.vercel.app/",
       tech:"Technologies: Typescript, NextJs, Tailwinds CSS, Vercel"
     },
     {
@@ -68,16 +68,8 @@ export const items = [
       title: "Shobie Online Shopping",
       img: "https://user-images.githubusercontent.com/49017322/284315779-f2aa96ee-528b-4cde-a37e-4d771183c4f9.png",
       desc: "This is a simple UI/UX design for an online shopping website.",
-      demo:"https://ux-ui-shobie.vercel.app/",
+      demo:"https://ux-ui-shobie-git-main-juliathtrannguyens-projects.vercel.app/",
       tech:"Technologies: Typescript, NextJs, Tailwinds CSS, Vercel"
-    },
-    {
-      id: 9,
-      title: "JuJu Note App",
-      img: "https://github.com/JuliaThTranNguyen/NoteApp/assets/49017322/c05fc3b2-de80-49d8-8a4c-d78929a42694",
-      desc: "This is a simple note-taking app. Practice codes for interviews.",
-      demo:"https://note-app-rose-three.vercel.app/",
-      tech:"Technologies: Typescript, Material UI, SCSS, Vite, Vercel"
     },
   ];
   
